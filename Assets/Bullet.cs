4f80740d24724847cb0c97c80bd6e7e5285ea32a
AamirMoBad <aamir.mbad@gmail.com>
2016-11-23 16:48:29 -0500

    Initial Commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { 
+        var hit = collision.gameObject;
+        var hitPlayer = hit.GetComponent<Combat>();
+        if(hitPlayer != null)
+        {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Combat.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Combat : NetworkBehaviour {
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar]
+    public int health = maxHealth;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TakeDamage(int amount)
+    {
+        if(!isServer)
+            return;
+        health -= amount;
+        if (health <= 0)
+        {
+            if (destroyOnDeath)
+                Destroy(gameObject);
+            else
+            {
+                health = maxHealth;
+                RpcRespawn();
+            }
+        }
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (isLocalPlayer)
+            transform.position = Vector3.zero;
+    }
+}
